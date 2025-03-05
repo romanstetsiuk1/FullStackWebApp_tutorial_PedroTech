53defd57e43dec5ca12decd98ad8351c9dde7771
Roman Stetsiuk <romansteciuk021@gmail.com>
2025-03-05 15:04:39 +0100

    Wyswietlanie postow pobranych z bazy danych.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import pliku CSS
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
+
+    const [ListOfPosts, setListOfPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/posts").then((response) => {
+            setListOfPosts(response.data);
+        });
+    },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {ListOfPosts.map((value,key) => {
+        return (
+        <div className='post'>
+            <div className='title'>{value.title}</div>
+            <div className='postText'>{value.postText}</div>
+            <div className='userName'>{value.userName}</div>
+        </div>);
+        })}
     </div>
   );
 }
---
+++ client/webpack.config.js
@@ -0,0 +1,45 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+// Uzyskiwanie ścieżki do bieżącego katalogu
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/, // Reguła dla plików CSS
+        use: ['style-loader', 'css-loader'], // Użyj style-loader i css-loader
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
+};
